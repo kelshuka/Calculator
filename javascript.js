5d545888507c6a79d0162d9895ca256fc3e9f7dd
Ukam <ukam@Xubuntu.myguest.virtualbox.org>
2024-03-06 22:02:41 +0000

    Add index.html and javascript.js

--- javascript.js
+++ javascript.js
@@ -40,27 +40,52 @@ const display = document.querySelector('#display');
 
 let displayValue = 0;
 
-let firstNum = 0;
-let operSign = '';
-let secondNum =0;
-
-
 
 const numbs = document.querySelectorAll('.numbs');
 
+
 numbs.forEach(contents => {
     contents.addEventListener('click', (e) => {
-        displayUpdate(e);
-    })
+        displayValue += e.target.value;
+        display.innerText = displayValue;
+
+        if(e.target.value=="+"||e.target.value=="-"||e.target.value=="*"||e.target.value=="/") {
+            console.log("matched");
+            display.innerText = display.innerText.slice(1, display.innerText.length);
+        }
+        console.log(e.target.value + " clicked");
 })
-
-let displayUpdate = (e) => {
-    displayValue += e.target.id;
-    display.innerText = displayValue;
-};
+});
 
 
 
+const butnEquals = document.querySelector('#equals');
+
+let numArr = [];
+//let operator;
+let result = 0;
+let prevValue = 0;
+
+function calculate() {
+    butnEquals.addEventListener('click', () => {
+        let strng = display.innerText;
+        numArr = strng.split(/(?<=[-+*\/])|(?=[-+*\/])/);
+        console.log(numArr);
+        try{
+            while(numArr.length!=1){
+            result = operate(numArr[1],parseFloat(numArr[0]),parseFloat(numArr[2]));
+            numArr.splice(0,2);
+            numArr[0] = result;
+            display.innerText = numArr.join("");
+            console.log("inside while");
+            }
+            display.innerText = result;
+            console.log(result);
+            //console.log(numArr);
+            console.log(display);
+        }catch(err){alert("error!!");}
+    });
+}
 
 const butnClear = document.querySelector('#clean');
 
@@ -74,3 +99,18 @@ butnClear.addEventListener('click', function(){
     
 })
 
+calculate();
+clearDisp();
+
+
+//numbs.forEach(contents => {
+ //   contents.addEventListener('click', (e) => {
+        //displayUpdate(e);
+ //   })
+//})
+
+
+//let displayUpdate = (e) => {
+ //   displayValue += e.target.value;
+ //   display.innerText = displayValue;
+//};
